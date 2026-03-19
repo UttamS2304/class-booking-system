02004869bcee1ee08ea69e573e2de3564af6f048
UttamS2304 <uttamsaxena2024@gmail.com>
2026-03-19 16:45:51 +0530

    2_Register.py

--- pages/2_Register.py
+++ pages/2_Register.py
@@ -3,18 +3,18 @@ import streamlit as st
 st.set_page_config(page_title="Register", layout="wide")
 
 st.title("Registration")
-st.write("Choose the type of registration")
+st.write("Choose your role")
+
+st.markdown("---")
 
 col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("Sales Person")
-    st.write("Register as a sales person for Creative Kids or Little Genius.")
-    if st.button("Go to Sales Registration"):
+    if st.button("Register as Sales Person"):
         st.switch_page("pages/3_Sales_Register.py")
 
 with col2:
     st.subheader("Resource Person")
-    st.write("Register as a resource person and select your subjects.")
-    if st.button("Go to Resource Registration"):
+    if st.button("Register as Resource Person"):
         st.switch_page("pages/4_Resource_Register.py")
