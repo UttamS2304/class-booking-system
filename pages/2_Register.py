c5a839fb701c2d07bd6baf715cf84fad8b72abd2
UttamS2304 <uttamsaxena2024@gmail.com>
2026-03-19 16:42:57 +0530

    2_Register.py
    Updated the registration page layout and added buttons for navigation.

--- pages/2_Register.py
+++ pages/2_Register.py
@@ -1,12 +1,20 @@
 import streamlit as st
 
-st.title("Register")
-st.write("Choose registration type")
+st.set_page_config(page_title="Register", layout="wide")
+
+st.title("Registration")
+st.write("Choose the type of registration")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.page_link("pages/3_Sales_Register.py", label="Sales Person Registration")
+    st.subheader("Sales Person")
+    st.write("Register as a sales person for Creative Kids or Little Genius.")
+    if st.button("Go to Sales Registration"):
+        st.switch_page("pages/3_Sales_Register.py")
 
 with col2:
-    st.page_link("pages/4_Resource_Register.py", label="Resource Person Registration")
+    st.subheader("Resource Person")
+    st.write("Register as a resource person and select your subjects.")
+    if st.button("Go to Resource Registration"):
+        st.switch_page("pages/4_Resource_Register.py")
