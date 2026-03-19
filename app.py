91781cf036d722b3b397ea8f18b3ec3cfb182907
UttamS2304 <uttamsaxena2024@gmail.com>
2026-03-19 16:16:28 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
+from supabase import create_client
 
 st.set_page_config(page_title="Class Booking System", layout="wide")
 
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 st.title("Class Booking System")
-st.write("Project setup successful")
+st.success("Supabase connected successfully")
