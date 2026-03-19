66978b228d4c9c21e6fd3251817182a9807dfc5a
UttamS2304 <uttamsaxena2024@gmail.com>
2026-03-19 16:48:25 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-# UI
 st.title("Class Booking System")
 st.write("Welcome to the class booking platform.")
 
@@ -19,11 +18,9 @@ col1, col2 = st.columns(2)
 with col1:
     st.subheader("Login")
     st.write("Already registered? Login here.")
-    if st.button("Go to Login"):
-        st.switch_page("pages/1_Login.py")
+    st.page_link("pages/1_Login.py", label="Go to Login")
 
 with col2:
     st.subheader("Register")
     st.write("New user? Create an account.")
-    if st.button("Go to Register"):
-        st.switch_page("pages/2_Register.py")
+    st.page_link("pages/2_Register.py", label="Go to Register")
